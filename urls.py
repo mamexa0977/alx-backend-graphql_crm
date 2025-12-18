1a5dd372106f52ac407f012ace71644840a66b34
mamexa0977 <mamexabdela@gmail.com>
2025-12-18 16:03:07 +0300

    fix settings.py

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from alx_backend_graphql_crm.schema import schema
+from alx_backend_graphql.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
